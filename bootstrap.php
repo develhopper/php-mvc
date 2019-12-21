075e686dcc2e189d97544a1cedb02a405851ffac
develhopper <develhoper.0@gmail.com>
2019-12-21 09:56:43 +0330

    autoloading vendors

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 const BASEDIR = __DIR__;
 require_once 'App/core/config.php';
+if(file_exists(__DIR__."/vendor/autoload.php")){
+	require_once __DIR__ . "/vendor/autoload.php";
+}
 spl_autoload_register(function ($name) {
 	$filename = BASEDIR . DIRECTORY_SEPARATOR . str_replace('\\', '/', $name) . '.php';
 	if (file_exists($filename)) {
